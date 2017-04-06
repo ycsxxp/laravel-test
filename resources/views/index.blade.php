c4730ebad69dd63eec8a4bf9c31bb8d4beab87e5
ycsxxp <ycsxxp@126.com>
2017-04-07 01:26:00 +0800

    udate

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div id="app">
+		<example />
+	</div>
+	<script src="{{ URL::asset('/') }}js/app.js"></script>
+</body>
+</html>

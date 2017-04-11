d6e66b84a81b94f5584aba9f5e9ba8424ac0fec7
yangchao <ycsxxp@users.noreply.github.com>
2017-04-11 19:08:08 +0800

    update

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -14,6 +14,7 @@ require('./bootstrap');
  */
 
 Vue.component('example', require('./components/Example.vue'));
+// Vue.component('login', require('./components/Login.vue'));
 
 const app = new Vue({
     el: '#app'
